ebf7bec89536b2f6a013887629f30b4afcf479c5
jonkahana <jonkahana1996@gmail.com>
2022-05-16 18:26:05 +0300

    change to match red_panda

--- bash_scripts/cars3d__id_az.sh
+++ bash_scripts/cars3d__id_az.sh
@@ -2,10 +2,10 @@
 
 False=''
 True='True'
-dataset=cars3d__az_id__train
+dataset=cars3d__id_az__train
 
 python -u main.py \
---dataset=cars3d__az_id__train \
+--dataset=cars3d__id_az__train \
 --backbone=50 \
 
 
