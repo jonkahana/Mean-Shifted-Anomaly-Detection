d7fc77b0bc32df83be414065e5afc98c88f3f3c1
jonkahana <jonkahana1996@gmail.com>
2022-05-17 21:46:15 +0300

    run all exps of mean_shifted on valid exps

--- run_job_array.sh
+++ run_job_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=mean_shifted__all
-#SBATCH --exclude=firth-02
+#SBATCH --exclude=firth-02,firth-01
 #SBATCH --cpus-per-task=4
 #SBATCH --array=1-22%22
 #SBATCH --output=slurm_trash/job_%A_%a
